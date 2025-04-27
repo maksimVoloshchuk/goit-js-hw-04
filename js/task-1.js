21aa700db45519ebb71c196c78c44cc37e52d272
maksimVoloshchuk <maksimvolosuk684@gmail.com>
2025-04-27 23:54:45 +0200

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,38 +1,15 @@
 function isEnoughCapacity(products, containerSize) {
-    const values = [];
-    for (const key in products) {
-         values.push(products[key]);
-       };
-       let sum = 0;
-       for (let item of values) {
-             sum += item;
-            };
-        if(sum <= containerSize) {
-              return true;
-        } else {
-            return false;
-        }
-      };
-     
+  const totalQuantity = Object.values(products).reduce(
+    (sum, quantity) => sum + quantity,
+    0
+  );
+  return totalQuantity <= containerSize;
+}
 
+console.log(isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)); // true
 
+console.log(isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)); // false
 
+console.log(isEnoughCapacity({ apples: 1, lime: 5, tomatoes: 3 }, 14)); // true
 
-
-
-
-console.log(
-    isEnoughCapacity({ apples: 2, grapes: 3, carrots: 1 }, 8)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 4, grapes: 6, lime: 16 }, 12)
-  ); // false
-  
-  console.log(
-    isEnoughCapacity({ apples: 1, lime: 5, tomatos: 3 }, 14)
-  ); // true
-  
-  console.log(
-    isEnoughCapacity({ apples: 18, potatos: 5, oranges: 2 }, 7)
-  ); // false
+console.log(isEnoughCapacity({ apples: 18, potatoes: 5, oranges: 2 }, 7)); // false
